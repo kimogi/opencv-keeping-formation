f246b2d09df5b390f297dcf89a5dad72bdcf4bc7
kimogi <doktorleibniz@gmail.com>
2015-04-19 12:18:03 +0000

    multiple frame capture added

--- camera.cpp
+++ camera.cpp
@@ -1,5 +1,6 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <ctime>
 #include <iostream>
 
 using namespace cv;
@@ -16,23 +17,28 @@ int main(int argc, char* argv[]) {
 //	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
 //	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 
-//   while (1)
-//  {
-	Mat frame;
+	while (1) {
+		Mat frame;
 
-	bool bSuccess = cap.read(frame); // read a new frame from video
+		bool bSuccess = cap.read(frame);
 
-	if (!bSuccess) {
-		cout << "Cannot read a frame from video stream" << endl;
-	//      break;
+		if (!bSuccess) {
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+
+		Mat mat_rotation = getRotationMatrix2D(Point(frame.rows/2, frame.cols/2), 180, 1);
+
+		Mat rotated_frame;
+		warpAffine(frame, rotated_frame, mat_rotation, frame.size());	
+
+		//time_t now = time(nullptr);
+		ostringstream nameStrStream;
+  		nameStrStream << "img/image_" << time(NULL) << ".jpg";
+  		string name = nameStrStream.str();
+	
+		imwrite(name, rotated_frame);	
+		cout << "New frame written to " << name << endl;
 	}
-
-	Mat mat_rotation = getRotationMatrix2D(Point(frame.rows/2, frame.cols/2), 180, 1);
-
-	Mat rotated_frame;
-	warpAffine(frame, rotated_frame, mat_rotation, frame.size());	
-
-	imwrite( "image.jpg", rotated_frame);	
-// }
 	return 0;
 }

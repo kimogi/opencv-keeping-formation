4babd870e035a4d17daa9d2a7b3f4fadec2e16ee
kimogi <doktorleibniz@gmail.com>
2015-05-31 11:52:50 +0000

    frame skipping added, small bits clearing excluded

--- camera/camera.cpp
+++ camera/camera.cpp
@@ -61,18 +61,7 @@ int main(int argc, char* argv[]) {
 	wiringPiSetup();
   	pinMode(LED_SWITCH, OUTPUT);	
 	
-	int l=0;
-	for (l=0; l<5; l++)
-	{
-		digitalWrite(LED_SWITCH, HIGH);
-		cout << "led on\n" << endl;
-		sleep(5);
-		digitalWrite(LED_SWITCH, LOW);
-		cout << "led off\n" << endl;
-		sleep(5);
-	}		
-	
-	VideoCapture cap(0); // open the video camera no. 0
+	VideoCapture cap(0);
 
 	if (!cap.isOpened()) {
 		cout << "Cannot open the video cam" << endl;
@@ -81,57 +70,61 @@ int main(int argc, char* argv[]) {
 
 	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
 	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
-
-//	while (1) {
+	
+	int count = -1;
+	while (1) {
+		cout << "begin " << time(0) << endl;
 		Mat image;
-
+		
 		bool bSuccess = cap.read(image);
-
 		if (!bSuccess) {
 			cout << "Cannot read a frame from video stream" << endl;
-			//break;
 			return 0;
 		}
-
+		count++;
+		if (count % 10 != 0)
+		{
+			continue;
+		}
+	
 		Mat matRotation = getRotationMatrix2D(Point(dWidth/2, dHeight/2), 180, 1);
 
 		Mat imgRotated;
 		warpAffine(image, imgRotated, matRotation, image.size());	
 
 		string path = "img/";
-		imwrite(path + "org_image" + ".jpg", imgRotated);	
+		//imwrite(path + "org_image" + ".jpg", imgRotated);	
 
 		Mat imgHSV;
-                cvtColor(image, imgHSV, COLOR_BGR2HSV);
+                cvtColor(imgRotated, imgHSV, COLOR_BGR2HSV);
 
 		Mat imgBlueOnly;	
-		inRange(imgHSV, Scalar(180, 50, 50), Scalar(280, 255, 255), imgBlueOnly);
+		inRange(imgHSV, Scalar(100, 50, 50), Scalar(140, 255, 255), imgBlueOnly);
 
-		//morphological opening (remove small objects from the foreground)
-  		erode(imgBlueOnly, imgBlueOnly, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-  		dilate(imgBlueOnly, imgBlueOnly, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+  		//erode(imgBlueOnly, imgBlueOnly, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+  		//dilate(imgBlueOnly, imgBlueOnly, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
 
-   		//morphological closing (fill small holes in the foreground)
-  		dilate(imgRedOnly, imgRedOnly, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-  		erode(imgRedOnly, imgRedOnly, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );			
+  		//dilate(imgBlueOnly, imgBlueOnly, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+  	//	erode(imgBlueOnly, imgBlueOnly, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );			
 
-		imwrite(path + "blue_image" + ".jpg", imgBlueOnly);
+		//imwrite(path + "blue_image" + ".jpg", imgBlueOnly);
 
 		Mat imgBounded = imgRotated.clone();
 		double obj_width = bound(imgBlueOnly, imgBounded);
 		
 		double obj_width_px = (2592/640)*obj_width;
-		cout << "width : " << obj_width_px << " pixels" << endl;		
+	//	cout << "width : " << obj_width_px << " pixels" << endl;		
 		double width_mm = 1.4 * obj_width_px/1000;
-		cout << "width : " << width_mm << " mm" << endl;
+	//	cout << "width : " << width_mm << " mm" << endl;
 		double real_width = 50;
-		cout << "real width : " << real_width << " mm" << endl;
+	//	cout << "real width : " << real_width << " mm" << endl;
 		double focal_len_mm = 3.6;
-		cout << "focal length : " << focal_len_mm << " mm" << endl;
+	//	cout << "focal length : " << focal_len_mm << " mm" << endl;
 		double distance = (real_width/width_mm)	* focal_len_mm;
 		cout << "distance : " << distance << " mm" << endl;	
 
-		imwrite(path + "bounded_image" + ".jpg", imgBounded);
-//	}
+		//imwrite(path + "bounded_image" + ".jpg", imgBounded);
+		cout << "end " << time(0) << endl;
+	}
 	return 0;
 }
